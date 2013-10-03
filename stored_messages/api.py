648f975fb4aec4eb38bdab9840469da73d1b8550
Massimiliano Pippi <masci@evonove.it>
2013-10-03 15:13:11 +0200

    stub for message api

---
+++ stored_messages/api.py
@@ -0,0 +1,13 @@
+
+def add_message(users, level, message, extra_tags='', fail_silently=False):
+    """
+    Send a message to a list of users
+    """
+    pass
+
+
+def broadcast_message(level, message, extra_tags='', fail_silently=False):
+    """
+    Send a message to all users in the system
+    """
+    pass
