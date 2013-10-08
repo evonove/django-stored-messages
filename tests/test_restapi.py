8a91397b277223c91f8bf2746fece8719025e560
Massimiliano Pippi <masci@evonove.it>
2013-10-08 10:02:11 +0200

    skip tests if restframework is not installed

--- tests/test_restapi.py
+++ tests/test_restapi.py
@@ -1,13 +1,21 @@
 from . import BaseTest
 
 from django.core.urlresolvers import reverse
+from unittest import skipUnless
 
 import json
 
 from stored_messages.constants import STORED_ERROR
 
+try:
+    import rest_framework
+    rest_framework_installed = True
+except ImportError:
+    rest_framework_installed = False
+
 
 class TestRESTApi(BaseTest):
+    @skipUnless(rest_framework_installed, "Django restframework is not installed")
     def test_retrieve(self):
         self.client.login(username='test_user', password='123456')
         self.client.get('/create')
@@ -20,6 +28,7 @@ class TestRESTApi(BaseTest):
         self.assertEqual(messages[0]['message']['level'], STORED_ERROR)
         self.assertEqual(messages[1]['message']['level'], STORED_ERROR)
 
+    @skipUnless(rest_framework_installed, "Django restframework is not installed")
     def test_make_read(self):
         self.client.login(username='test_user', password='123456')
         self.client.get('/create')
@@ -32,4 +41,3 @@ class TestRESTApi(BaseTest):
         r = self.client.get(reverse('stored_messages:inbox-list'))
         messages = json.loads(r.content.decode('utf-8'))
         self.assertEqual(len(messages), 0)
-
