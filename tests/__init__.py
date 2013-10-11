e639a472a281eb5f4a0f2a8552071fcbb50a1c9b
Federico Frenguelli <synasius@gmail.com>
2013-10-11 13:57:02 +0200

    Add factory to BaseTest

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,6 +9,8 @@ class BaseTest(TestCase):
     urls = 'tests.urls'
 
     def setUp(self):
+        self.factory = RequestFactory()
+
         self.user = get_user_model().objects.create_user("test_user", "t@user.com", "123456")
         self.request = RequestFactory().get('/')
         self.request.session = mock.MagicMock()
