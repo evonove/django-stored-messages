a0de66d9b8a1d604c1a3d1369904b88b781be045
Massimiliano Pippi <masci@evonove.it>
2013-10-15 14:36:20 +0200

    added template tag for archived messages

--- stored_messages/templatetags/stored_messages_tags.py
+++ stored_messages/templatetags/stored_messages_tags.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django import template
 
-from ..models import Inbox
+from ..models import Inbox, MessageArchive
 
 
 register = template.Library()
@@ -11,11 +11,23 @@ register = template.Library()
 @register.inclusion_tag("stored_messages/stored_messages_list.html", takes_context=True)
 def stored_messages_list(context, num_elements=10):
     """
-    Renders a list of stored messages for the current user
+    Renders a list of unread stored messages for the current user
     """
     if "user" in context:
         user = context["user"]
         if user.is_authenticated():
             return {
                 "messages": Inbox.objects.select_related("message").filter(user=user)[:num_elements]
+            }
+
+@register.inclusion_tag("stored_messages/stored_messages_list.html", takes_context=True)
+def stored_messages_archive(context, num_elements=10):
+    """
+    Renders a list of archived messages for the current user
+    """
+    if "user" in context:
+        user = context["user"]
+        if user.is_authenticated():
+            return {
+                "messages": MessageArchive.objects.select_related("message").filter(user=user)[:num_elements]
             }
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -10,15 +10,25 @@ import stored_messages
 
 
 class TestStoredMessagesTags(BaseTest):
-    def test_stored_messages_list(self):
+    def _create_messages(self):
         # create a message
         for i in range(20):
             stored_messages.add_message_for([self.user], stored_messages.INFO, "unicode message ❤")
 
+    def test_stored_messages_list(self):
+        self._create_messages()
+
         t = Template("{% load stored_messages_tags %}"
                      "{% stored_messages_list 15 %}")
         render = t.render(RequestContext(self.request))
+        self.assertInHTML("<li>[test_user] unicode message ❤</li>", render, 15)
 
+    def test_stored_messages_archive(self):
+        self._create_messages()
+
+        t = Template("{% load stored_messages_tags %}"
+                     "{% stored_messages_archive 15 %}")
+        render = t.render(RequestContext(self.request))
         self.assertInHTML("<li>[test_user] unicode message ❤</li>", render, 15)
 
     def test_stored_messages_list_empty_for_unauthenticated_user(self):
@@ -26,6 +36,15 @@ class TestStoredMessagesTags(BaseTest):
 
         t = Template("{% load stored_messages_tags %}"
                      "{% stored_messages_list 15 %}")
+        render = t.render(RequestContext(self.factory.get("/")))
+
+        self.assertInHTML("<li>[test_user] unicode message ❤</li>", render, 0)
+
+    def test_stored_messages_archive_empty_for_unauthenticated_user(self):
+        stored_messages.add_message_for([self.user], stored_messages.INFO, "unicode message ❤")
+
+        t = Template("{% load stored_messages_tags %}"
+                     "{% stored_messages_archive 15 %}")
         render = t.render(RequestContext(self.factory.get("/")))
 
         self.assertInHTML("<li>[test_user] unicode message ❤</li>", render, 0)
