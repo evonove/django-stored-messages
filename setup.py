5fac3d73b5532639c797a0c9caf7a9d7bd8e0759
Massimiliano Pippi <masci@evonove.it>
2013-11-02 12:40:00 +0100

    updated classification to Beta status

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     zip_safe=False,
     keywords='django-stored-messages',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
