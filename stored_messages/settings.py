fd7e402d110b2f9a561cbf0db8daae9898ae8425
masci <mpippi@gmail.com>
2014-03-24 19:34:48 +0100

    default backend class

--- stored_messages/settings.py
+++ stored_messages/settings.py
@@ -39,6 +39,7 @@ DEFAULTS = {
         STORED_ERROR: 'stored error',
     }),
     'INBOX_EXPIRE_DAYS': 30,
+    'STORAGE_BACKEND': 'backends.django_orm.backend',
 }
 
 # List of settings that cannot be empty
