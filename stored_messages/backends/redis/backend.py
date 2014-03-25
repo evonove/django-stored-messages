8a09610d3368181373582e9eb28c264cf6a926f7
Massimiliano Pippi <mpippi@gmail.com>
2014-03-26 00:11:30 +0100

    more on redis backend

--- stored_messages/backends/redis/backend.py
+++ stored_messages/backends/redis/backend.py
@@ -1,41 +1,35 @@
+import redis
 
-class RedisBackend(object):
+from ..base import StoredMessagesBackend
+from ...settings import stored_messages_settings
+
+
+class RedisBackend(StoredMessagesBackend):
     """
 
     """
+    def __init__(self):
+        self.client = redis.StrictRedis(host=stored_messages_settings.REDIS_HOST,
+                                        port=stored_messages_settings.REDIS_PORT,
+                                        db=stored_messages_settings.REDIS_DB)
+
     def inbox_list(self, user):
-        """
-        Retrieve all the messages in `user`'s Inbox
-        """
-        raise NotImplementedError
+        return self.client.get('user:%d:notifications' % user.pk)
 
     def inbox_purge(self, user):
-        """
-        Delete all the messages in `user`'s Inbox
-        """
-        raise NotImplementedError
+        self.client.delete('user:%d:notifications' % user.pk)
 
-    def inbox_get_or_create(self, user, msg_text):
-        """
-        Create or retrieve an `user`'s Inbox entry containing `msg_text`
-        """
-        raise NotImplementedError
+    def inbox_get_or_create(self, user, message):
+        self.client.sadd('user:%d:notifications' % user.pk, message)
 
     def create_message(self, user, msg_text, level, extra_tags):
-        """
-        Create a Message instance
-        """
-        raise NotImplementedError
+        m = {'message': msg_text, 'level': level, 'tags': extra_tags}
+        self.client.sadd('user:%d:archive' % user.pk, m)
+        return m
 
     def archive_list(self, user):
-        """
-        Retrieve all the messages in `user`'s archive
-        """
-        raise NotImplementedError
+        return self.client.get('user:%d:archive' % user.pk)
 
     def can_handle(self, msg_instance):
-        """
-        Determine if this backend can handle message of type `msg_instance`
-        """
-        raise NotImplementedError
-
+        return (isinstance(msg_instance, dict) and
+                msg_instance.keys() == ('message', 'level', 'tags'))
