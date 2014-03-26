e890e0940184b8a0dce2d5d3a04b2a19c9948847
Massimiliano Pippi <mpippi@gmail.com>
2014-03-26 18:04:01 +0100

    fixed redis backend for the new api

--- stored_messages/backends/redis/backend.py
+++ stored_messages/backends/redis/backend.py
@@ -1,5 +1,6 @@
 import redis
 
+from ..exceptions import MessageTypeNotSupported
 from ..base import StoredMessagesBackend
 from ...settings import stored_messages_settings
 
@@ -19,13 +20,24 @@ class RedisBackend(StoredMessagesBackend):
     def inbox_purge(self, user):
         self.client.delete('user:%d:notifications' % user.pk)
 
-    def inbox_get_or_create(self, user, message):
-        self.client.sadd('user:%d:notifications' % user.pk, message)
+    def inbox_store(self, users, msg_instance):
+        if not self.can_handle(msg_instance):
+            raise MessageTypeNotSupported()
+
+        for user in users:
+            self.client.sadd('user:%d:notifications' % user.pk, msg_instance)
 
     def create_message(self, user, msg_text, level, extra_tags):
         m = {'message': msg_text, 'level': level, 'tags': extra_tags}
-        self.client.sadd('user:%d:archive' % user.pk, m)
+
         return m
+
+    def archive_store(self, users, msg_instance):
+        if not self.can_handle(msg_instance):
+            raise MessageTypeNotSupported()
+
+        for user in users:
+            self.client.sadd('user:%d:archive' % user.pk, msg_instance)
 
     def archive_list(self, user):
         return self.client.get('user:%d:archive' % user.pk)
