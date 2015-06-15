84a4a0ce64f6b011e55dfe49f497ae0c5ad0372a
Massimiliano Pippi <mpippi@gmail.com>
2015-06-15 11:58:24 +0200

    better assert

--- stored_messages/tests/test_models.py
+++ stored_messages/tests/test_models.py
@@ -13,7 +13,9 @@ class TestInbox(BaseTest):
     def test_expire(self):
         self.request._messages = default_storage(self.request)
         add_message(self.request, STORED_ERROR, "an SOS to the world ☢")
-        ma = MessageArchive.objects.all()[0]
+        mas = MessageArchive.objects.all()
+        self.assertEqual(len(mas), 1)
+        ma = mas[0]
         ma.message.date = now() - timedelta(days=365)
         ma.message.save()
         inbox = Inbox.objects.create(user=ma.user, message=ma.message)
