320e15cfb504d6125e9cfdf1640813513e0941f3
Massimiliano Pippi <mpippi@gmail.com>
2015-06-15 23:43:22 +0200

    temporary increase verbosity to debug travis builds

--- runtests.py
+++ runtests.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stored_messages.tests.settings")
 
     from django.core.management import execute_from_command_line
-    execute_from_command_line([sys.argv[0], "test", app_to_test])
+    execute_from_command_line([sys.argv[0], "test", app_to_test, '-v', '3'])
