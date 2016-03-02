c9978ce1aecdd070ab12f92070d6bfdb86c46661
Emanuele <emanuele.palazzetti@gmail.com>
2016-03-02 20:44:57 +0100

    using a plain list for urlpatterns

--- stored_messages/tests/urls.py
+++ stored_messages/tests/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls import patterns, url, include
 from django.contrib import admin
+from django.conf.urls import url, include
+
+from stored_messages.tests.views import message_view, message_create, message_create_mixed
+
+
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^consume$', 'stored_messages.tests.views.message_view'),
-    url(r'^create$', 'stored_messages.tests.views.message_create'),
-    url(r'^create_mixed$', 'stored_messages.tests.views.message_create_mixed'),
+urlpatterns = [
+    url(r'^consume$', message_view),
+    url(r'^create$', message_create),
+    url(r'^create_mixed$', message_create_mixed),
     url(r'^messages', include('stored_messages.urls', namespace='stored_messages'))
-)
+]
--- stored_messages/urls.py
+++ stored_messages/urls.py
@@ -5,14 +5,13 @@ from django.conf import settings
 
 if 'rest_framework' in settings.INSTALLED_APPS:
     from rest_framework.routers import DefaultRouter
-    from django.conf.urls import patterns, url, include
+    from django.conf.urls import url, include
     from . import views
 
     router = DefaultRouter()
     router.register(r'inbox', views.InboxViewSet, base_name='inbox')
 
-    urlpatterns = patterns(
-        '',
+    urlpatterns = [
         url(r'^', include(router.urls)),
         url(r'^mark_all_read/$', views.mark_all_read, name='mark_all_read'),
-    )
+    ]
